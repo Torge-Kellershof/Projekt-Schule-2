91706bd612a8598231025da50182affff085d355
Torge-Kellershof <Torge.Kellershof@gmx.de>
2021-02-17 07:01:08 +0100

    gestern

---
+++ Java/service/userService2-1.js
@@ -0,0 +1,12 @@
+class UserService {
+
+//constructor(){}
+
+static displayUser(user)        {
+  return "<tr><td>" + user.id + "</tr></td>"
+//  return "displayUser";     }
+
+
+static changePassword(oldPassword, newPassword, repeatenewPassword){
+  return "passwordChanged";                                         }
+}
---
+++ Java/user.js
@@ -0,0 +1,6 @@
+//
+class User {
+//  var id; var name; var password; var email;
+  constructor(id, name, password,email) {
+    this.id=id; this.name=name; this.password=password; this.email=email;
+  }}
